a9fca9e9dd8ddd4dc2b524a7941c8a92cae2ebdd
x3nom <jakub.p12345678910@gmail.com>
2025-01-27 21:42:24 +0100

    working as cli tool

--- src/build-dict.py
+++ src/build-dict.py
@@ -5,12 +5,19 @@ with open("65536-words.csv", "r") as f:
     content = f.readlines()
 
 
-word_dict = {}
+hex_word = {}
+word_hex = {}
 
 for line in content:
     hx, word = line.split(',')
-    word_dict.update({hx.strip() : word.strip().lower()})
+    hx, word = hx.strip().lower(), word.strip().lower()
+    
+    hex_word.update({hx : word})
+    word_hex.update({word : hx})
 
 
 with open("word-dict.pickle", "wb") as f:
-    pickle.dump(word_dict, f)
+    pickle.dump({
+        "hex:word":hex_word,
+        "word:hex":word_hex
+        }, f)
